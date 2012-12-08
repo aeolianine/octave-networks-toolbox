821fe10a1f6eaa443d4db780ecd3bded7d7d825d
aeolianine <gergana.bounova@gmail.com>
2012-12-08 22:24:27 +0100

    adding the last two viz files | Dec 8 2012 10:24 pm

---
+++ edgeL2cyto.m
@@ -0,0 +1,23 @@
+##################################################################
+% Convert an edge list structure m x [node 1, node 2, link] to 
+%    Cytoscape input format (.txt or any text extension works)
+%
+% Note: In Cytoscape the column separator option is semi-colon ";". 
+%             If desired, this is easy to change below in line 20.
+%
+% INPUTs: edgelist - mx3 matrix, m = number of edges, file name text string
+% OUTPUTs: text file in Cytoscape format with a semicolon column separator
+##################################################################
+
+function [ ]=edgeL2cyto(el,filename)
+
+nodes=unique([el(:,1)', el(:,2)']);
+m = size(el,1);     % number of edges
+
+fid = fopen(filename,'wt','native');
+
+for i=1:m
+  fprintf(fid,'    %4i ;  %4i ;  %3.6f\n',el(i,1),el(i,2),el(i,3));
+end
+
+fclose(fid);
