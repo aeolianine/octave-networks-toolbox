c98ef7ddcb8b767d7ee09c677ed31b093469e292
Gergana <Gergana@gerganas-macbook.local>
2014-07-13 18:55:00 +0200

    adding adj2adjL() and changing sections order in the manual | July 13 2014 6:54pm

--- adj2adjL.m
+++ adj2adjL.m
@@ -1,4 +1,3 @@
-##################################################################
 % Convert an adjacency graph representation to an adjacency list.
 % Note 1: Valid for a general (directed, not simple) graph.
 % Note 2: Edge weights (if any) get lost in the conversion.
@@ -7,7 +6,6 @@
 % OUTPUT: cell structure for adjacency list: x{i_1}=[j_1,j_2 ...]
 %
 % GB: last updated, September 24 2012
-##################################################################
 
 function L = adj2adjL(adj)
 
