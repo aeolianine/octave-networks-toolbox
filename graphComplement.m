150fa3990fb95107e1f7097d56140f70b8bbc679
Gergana <gergana.bounova@gmail.com>
2014-10-04 14:02:39 +0200

    adding graphComplement.m to the documentation | Oct 4 2014 2:02pm

--- graphComplement.m
+++ graphComplement.m
@@ -1,13 +1,13 @@
-##################################################################
 % Returns the complement of a graph
 % The complement graph has the same nodes, but edges where the original graph doesn't and vice versa.
 % 
 % INPUTs: adj - original graph adjacency matrix, nxn
 % OUTPUTs: complement graph adjacency matrix, nxn
 %
-% Note: Assumes no multiple edges
-% GB: last updated, September 23, 2012
-##################################################################
+% Note 1: Assumes no multiple edges.
+% Note 2: To create a complement graph without self-loops,
+%         use adjC=ones(size(adj))-adj-eye(length(adj)); instead.
+% GB: last updated, October 4, 2014
 
 function adjC = graphComplement(adj)
 
