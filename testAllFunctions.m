f34e7f9cebf602499a1490a54e36959acf1239e1
Gergana <gergana.bounova@gmail.com>
2014-10-27 07:35:52 +0100

    adding isWeighted.m to documentation | Oct 27 7:35am

--- testAllFunctions.m
+++ testAllFunctions.m
@@ -700,3 +700,17 @@ for x=1:10
   
 end
 % ................................................
+
+
+% Testing isWeighted.m ...........................
+fprintf('testing isWeighted.m\n')
+assert(isWeighted(adj2edgeL(T{2}{2})),false)
+assert(isWeighted(adj2edgeL(T{3}{2})),true)
+
+assert(isWeighted(adj2edgeL(randomGraph(randi(5)+20,rand+0.1))),false)
+  
+assert(isWeighted(adj2edgeL(randomDirectedGraph(randi(5)+20,rand+0.1))),false)
+  
+assert(isWeighted([1,2,0.5; 1,3,1.5; 1,4,1]),true)
+assert(isWeighted([1,2,0.5; 1,3,1; 1,4,1]),true)
+% ................................................
