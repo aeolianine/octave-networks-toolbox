094b3c721c7bc9c2109fc638883b7e49e9e79e64
Gergana <gergana.bounova@gmail.com>
2015-02-28 21:48:54 +0100

    adding weightedRandomSample.m to the documentation | Feb 28 2015 9:48 pm

--- weightedRandomSample.m
+++ weightedRandomSample.m
@@ -1,4 +1,3 @@
-##################################################################
 % Weighted random sampling.
 % 
 % INPUTs: number of draws from a discrete distribution (n)
@@ -8,14 +7,17 @@
 %              according to the weights in W
 %
 % GB: last updated, Oct 31 2012
-##################################################################
 
 
 function s = weightedRandomSample(n,P,W)
 
 s = [];
 
-if abs(sum(W)-1)>10**(-8); printf('The probabilities do not sum up to 1.\n'); return; end
+if abs(sum(W)-1)>10**(-8); 
+    printf('The probabilities do not sum up to 1.\n');
+    s = 'probabilities do not sum up to 1';
+    return; 
+end
 
 % divide the unit interval into |P| segments each with length W_i
 unit = [0,W(1)];
@@ -27,6 +29,6 @@ end
 while length(s)<n
 
   lb = max(find(unit<rand));    % rand is in [unit(lb), unit(lb+1)]
-  s = [s P(lb)];             % pick P(lb)
+  s = [s P(lb)];                % pick P(lb)
 
 end
