606f2d33e536b01be1b34bb70b3e5f0740a37a3f
Gergana <gergana.bounova@gmail.com>
2015-07-13 21:29:29 +0200

    adding edgeL2cyto.m to the documentation | Jul 13 9:29 pm

--- edgeL2cyto.m
+++ edgeL2cyto.m
@@ -1,13 +1,11 @@
-##################################################################
 % Convert an edge list structure m x [node 1, node 2, link] to 
 %    Cytoscape input format (.txt or any text extension works)
 %
 % Note: In Cytoscape the column separator option is semi-colon ";". 
-%             If desired, this is easy to change below in line 20.
+%             If desired, this is easy to change below in line 18.
 %
 % INPUTs: edge list - mx3 matrix, m = number of edges, file name text string
 % OUTPUTs: text file in Cytoscape format with a semicolon column separator
-##################################################################
 
 function [ ]=edgeL2cyto(el,filename)
 
