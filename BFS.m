52de4caa262787f6b1e46c27e8d679d266d2f87c
aeolianine <gergana.bounova@gmail.com>
2023-05-21 23:15:39 +0200

    code formatting - 2

--- BFS.m
+++ BFS.m
@@ -10,33 +10,38 @@
 %          this tree is effectively a spanning tree of the
 %          entire graph.
 %
-% GB: last updated, Nov 8 2014
+% Last updated: Nov 8 2014
 
-function T=BFS(adjL,s,t)
+function T = BFS(adjL, s, t)
 
-discovered=[s];          % mark visited
-q=[s];                   % add to queue
-T=cell(1,length(adjL));   % initialize search path/tree
+    discovered = [s]; % mark visited
+    q = [s]; % add to queue
+    T = cell(1, length(adjL)); % initialize search path/tree
 
-if s==t
-    printf('BFS(): The start and the end node are the same.\n')
-    return
-end
-
-while not(isempty(q))
-    j=q(1); q=q(2:length(q));  % pop the front
-    neigh=adjL{j};
-    for nn=1:length(neigh)
-        if isempty(find(discovered==neigh(nn)))
-            T{j}=[T{j}, neigh(nn)];
-            if neigh(nn)==t
-                return
-            end
-            discovered=[discovered, neigh(nn)];
-            q=[q, neigh(nn)];
-        end
+    if s == t
+        printf('BFS(): The start and the end node are the same.\n')
+        return
     end
-end
 
-printf('BFS(): Target node not found.\n')
+    while not(isempty(q))
+        j = q(1); q = q(2:length(q)); % pop the front
+        neigh = adjL{j};
 
+        for nn = 1:length(neigh)
+
+            if isempty(find(discovered == neigh(nn)))
+                T{j} = [T{j}, neigh(nn)];
+
+                if neigh(nn) == t
+                    return
+                end
+
+                discovered = [discovered, neigh(nn)];
+                q = [q, neigh(nn)];
+            end
+
+        end
+
+    end
+
+    printf('BFS(): Target node not found.\n')
